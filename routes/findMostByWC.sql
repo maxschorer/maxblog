b876e48d6439872f4d8e3d7c133f6d7e940a36df
maxschorer <mcschorer@gmail.com>
2014-03-20 06:05:14 +0000

    Edited the characters table and related SQL to export the name and id in addition to previous fields

--- routes/findMostByWC.sql
+++ routes/findMostByWC.sql
@@ -1,12 +1,16 @@
 SELECT
+  t2.id,
   t2.name,
+  t2.code,
   array_to_string(array_agg(episode_words ORDER BY season_num, episode_num), ',') AS raw_cnt_by_ep,
   array_to_string(array_agg(mov_ave ORDER BY season_num, episode_num), ',') AS mov_ave_by_ep,
   array_to_string(array_agg(avg_per_episode ORDER BY season_num, episode_num), ',') AS cumul_ave_by_ep
 FROM 
 (
 SELECT
+  t.id,
   t.name,
+  t.code,
   t.season_num,
   t.episode_num,
   t.episode_words,
@@ -15,7 +19,9 @@ SELECT
 
 FROM (
 SELECT
+  c.character_id AS id,
   c.name,
+  c.code,
   s.num AS season_num,
   e.num AS episode_num,
   SUM(CASE WHEN l.num_words IS NULL THEN 0 ELSE l.num_words END) AS episode_words
@@ -25,10 +31,11 @@ FROM characters c
   LEFT JOIN scenes sc ON e.episode_id = sc.episode_id
   LEFT JOIN lines l ON sc.scene_id = l.scene_id
     AND c.character_id = l.character_id
-GROUP BY c.name, s.num, e.num
+WHERE c.name IS NOT NULL
+GROUP BY c.character_id, c.name, c.code, s.num, e.num
 ORDER BY s.num, e.num
 ) t
 ) t2
-GROUP BY t2.name
+GROUP BY t2.id, t2.name, t2.code
 ORDER BY SUM(t2.episode_words) DESC
 LIMIT 
--- routes/restAPI.js
+++ routes/restAPI.js
@@ -1,17 +1,18 @@
+/*jslint smarttabs:true */
+
 var pg = require('pg');
 var fs = require('fs');
 
 var CON_STRING = 'pg://ubuntu:bitpass0@localhost:5432/shows';
-var NUM_RESULTS = 49;
+var NUM_RESULTS = 50;
 
-client = new pg.Client(CON_STRING);
+var client = new pg.Client(CON_STRING);
 client.connect();
 
 exports.findMostByWC = function(req, res){
     var num = req.params.num || NUM_RESULTS;
     var queryString = fs.readFileSync('routes/findMostByWC.sql').toString();
     queryString += num + ';';
-    console.log(queryString);
 
     client.query(queryString, function (err, result) {
 	var output = result.rows;
@@ -19,22 +20,22 @@ exports.findMostByWC = function(req, res){
 	    var rawCntByEpNum = [];
 	    var movAveByEpNum = [];
 	    var cumulAveByEpNum = [];
-	    var rawCntByEpStr = output[i]['raw_cnt_by_ep'].split(',');
-	    var movAveByEpStr = output[i]['mov_ave_by_ep'].split(',');
-	    var cumulAveByEpStr = output[i]['cumul_ave_by_ep'].split(',');
+	    var rawCntByEpStr = output[i].raw_cnt_by_ep.split(',');
+	    var movAveByEpStr = output[i].mov_ave_by_ep.split(',');
+	    var cumulAveByEpStr = output[i].cumul_ave_by_ep.split(',');
 	    for (var j = 0; j < rawCntByEpStr.length; j++){
 		rawCntByEpNum.push(parseInt(rawCntByEpStr[j]));
 		movAveByEpNum.push(parseInt(movAveByEpStr[j]));
 		cumulAveByEpNum.push(parseInt(cumulAveByEpStr[j]));
 	    }
 
-	    output[i]['raw_cnt_by_ep'] = rawCntByEpNum;
-	    output[i]['mov_ave_by_ep'] = movAveByEpNum;
-	    output[i]['cumul_ave_by_ep'] = cumulAveByEpNum;
+	    output[i].rawCntByEp = rawCntByEpNum;
+	    output[i].movAveByEp = movAveByEpNum;
+	    output[i].cumulAveByEp = cumulAveByEpNum;
 	}
 	res.json(output, headers={ 'Content-Type': 'application/json' }, status=200);
     });
-}
+};
 
 exports.findByCharName = function (req, res) {
     var name = req.params.name;
@@ -43,7 +44,7 @@ exports.findByCharName = function (req, res) {
     client.query(queryString, function (err, result) {
 	res.json(result);
     });
-}
+};
 
 exports.findEpisodes = function (req, res) {
     var show_id = req.params.id;
@@ -53,4 +54,4 @@ exports.findEpisodes = function (req, res) {
     client.query(queryString, function (err, result) {
 	res.json(result.rows, headers={ 'Content-Type': 'application/json' }, status=200);
     });
-}
+};
